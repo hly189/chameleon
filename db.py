6185e96f470605142ac641c310491c5b721c13f4
Hoa Ly <lythaihoa2311992@gmail.com>
2020-04-02 22:19:36 -0400

    Add previlige for user

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python3  
- 
-from os 	import popen, system 
+#! /usr/bin/env python3
+
+from os 	import popen, system
 from socket	import gethostname
 from sys	import argv
 from getpass	import getpass
@@ -9,11 +9,11 @@ import pymysql
 import myloginpath
 
 #-------------------------------------------------------------
-# Functions 
+# Functions
 #-------------------------------------------------------------
-#---- get agrgs 
-def get_args(): 
-	
+#---- get agrgs
+def get_args():
+
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-start', action='store_true')
 	parser.add_argument('-stop', action='store_true')
@@ -21,12 +21,12 @@ def get_args():
 	parser.add_argument('-init', action='store_true')
 	parser.add_argument('-help', action='store_true')
 	parser.add_argument('-login', nargs='+')
-	
+
 	args = parser.parse_args()
 	return args
-	
-#---- db help 
-def dbHelp(): 
+
+#---- db help
+def dbHelp():
 	print("Usage %s -help, -start, -stop, -init, -restart, -login" % argv[0])
 	print("-help: Usage")
 	print("-start: Start Database")
@@ -35,109 +35,111 @@ def dbHelp():
 	print("-init: initialize user and password for chameleon")
 	print("-login <user> - login to user")
 
-#---- Set login without password 
-def setlogin(user): 
+#---- Set login without password
+def setlogin(user):
 	print("Set login for user: %s" % user)
-	dbLoginCommand = '/usr/local/mysql/bin/mysql_config_editor set --login-path=%s --user=%s --password' % (user, user) 
-	system(dbLoginCommand)
-	
-
-#---- db login 
-def dbLogin(user): 
-	dbLoginCommand = '/usr/local/mysql/bin/mysql --login-path=%s' % user 
+	dbLoginCommand = '/usr/local/mysql/bin/mysql_config_editor set --login-path=%s --user=%s --password' % (user, user)
 	system(dbLoginCommand)
 
-#---- db service 
-def dbService(command): 
+
+#---- db login
+def dbLogin(user):
+	dbLoginCommand = '/usr/local/mysql/bin/mysql --login-path=%s' % user
+	system(dbLoginCommand)
+
+#---- db service
+def dbService(command):
 	dbCommand = "sudo /usr/local/mysql/support-files/mysql.server %s" % command
 	popen(dbCommand).read()
 
 	checkRunning = popen("ps -ef | grep mysql | grep -v grep > /dev/null 2>&1; echo $?").read().strip()
-	
-	if int(checkRunning) == 0: 
+
+	if int(checkRunning) == 0:
 		print("MySQL started successfully")
-	else: 
-		if command == 'stop' and int(checkRunning) != 0: 
+	else:
+		if command == 'stop' and int(checkRunning) != 0:
 			print("MySQL stopped successfully")
-			return 
+			return
 		print("Falied to Start")
 
 #---- get db connection
-def getMySqlConnection(usr='root'): 
+def getMySqlConnection(usr='root'):
 	ipaddress   = "127.0.0.1"
 	charset     = "utf8mb4"
-	curtype     = pymysql.cursors.DictCursor 
-	
+	curtype     = pymysql.cursors.DictCursor
+
 	conf = myloginpath.parse(usr)
 	sqlCon 	    = pymysql.connect(**conf, host=ipaddress, charset=charset, cursorclass=curtype)
 	sqlCursor   = sqlCon.cursor()
-	return sqlCon, sqlCursor 
+	return sqlCon, sqlCursor
 
 #---- get db connection
 def createDbUser(user, password):
 	sqlConn, sqlCursor = getMySqlConnection()
-	createUserQuery = "CREATE USER IF NOT EXISTS '%s'@'localhost' IDENTIFIED BY '%s';"%(user, password)
-	try: 
-		sqlCursor.execute(createUserQuery)
-	except Exception as ex:
-		print("ERROR: Falied to create User: %s" % user)
-		exit() 
+	sqlQueryList = ["CREATE USER IF NOT EXISTS '%s'@'localhost' IDENTIFIED BY '%s';"%(user, password),
+                    "GRANT ALL PRIVILEGES ON * . * TO '%s'@'localhost';" %(user)]
+	
+	for sqlQuery in sqlQueryList:
+		try:
+			sqlCursor.execute(sqlQuery)
+		except Exception as ex:
+			print("ERROR: Falied to create User: %s" % user)
+			exit()
+
 
 #---- show
 def listDb():
 	sqlConn, sqlCursor = getMySqlConnection()
 	sqlQuery = "select host, user from mysql.user;"
-	
-	try: 
+
+	try:
 		sqlCursor.execute(sqlQuery)
 	except Exception as ex:
 		print("ERROR: Falied to fetch data")
-		exit() 
+		exit()
 	userList = sqlCursor.fetchall()
 	sqlCursor.close()
 	sqlConn.close()
-	
+
 	return userList
 
-def main(): 
+def main():
 	args = get_args()
-	
+
 	dbChameleonUser = 'chameleon'
 	dbChameleonPass = 'chameleon!23'
 
 
 	# Print out Usage
-	if args.help: 
-		dbHelp() 
-	
-	# Start MySQL 
-	if args.start: 
+	if args.help:
+		dbHelp()
+
+	# Start MySQL
+	if args.start:
 		dbService("start")
-	
-	# Stop MySQL 
-	if args.stop: 
+
+	# Stop MySQL
+	if args.stop:
 		dbService("stop")
-	
+
 	# Restart MySQL
-	if args.restart: 
+	if args.restart:
 		dbService("restart")
-	
-	# Login MySQL 
-	if args.login: 
+
+	# Login MySQL
+	if args.login:
 		dbLogin(args.login[0])
 	# Init database
 	if args.init:
-		# Set login-path for root 
+		# Set login-path for root
 		setlogin('root')
 		createDbUser(dbChameleonUser, dbChameleonPass)
-		# Set login-path for chameleon 
+		# Set login-path for chameleon
 		print("Password for chameleon is %s" % dbChameleonPass)
 		setlogin(dbChameleonUser)
 		userList = listDb()
-		
+
 		print("List of users:");
 		for user in userList:
 		    print(user)
 main()
-
-
